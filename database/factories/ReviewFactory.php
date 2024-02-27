3fee032ea83156d074043139bb0e86e366e009a7
William Davidson <wj.davidson@outlook.com>
2024-02-27 16:11:32 +0100

    added review migration and seeder and fixed booking factory with incorrect user ids

---
+++ database/factories/ReviewFactory.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Booking;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Model>
+ */
+class ReviewFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        $reviews = json_decode(file_get_contents(__DIR__ . "/../../resources/faker_reviews.json"), true);
+        $review = $reviews[rand(0, count($reviews) - 1)];
+        $booking = Booking::query()->inRandomOrder()->first();
+
+        return [
+            "rating" => $review["rating"],
+            "description" => $review["description"],
+            "user_id" => $booking->renter_id,
+            "car_id" => $booking->car_id,
+        ];
+    }
+}
---
+++ database/migrations/2024_02_27_132259_create_reviews_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('reviews', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->integer("rating");
+            $table->string("description");
+            $table->foreignId("car_id")->constrained("cars")->cascadeOnDelete();
+            $table->foreignId("user_id")->constrained("users")->cascadeOnDelete();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('reviews');
+    }
+};
