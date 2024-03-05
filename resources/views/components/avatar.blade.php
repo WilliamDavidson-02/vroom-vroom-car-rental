ddf056306b430d6d046b405a2df36b304011dde9
Fanny Karlsson <70578538+Karlsson2@users.noreply.github.com>
2024-03-05 16:16:57 +0100

    quickfix to avatar

--- resources/views/components/avatar.blade.php
+++ resources/views/components/avatar.blade.php
@@ -1,4 +1,4 @@
 <div id="image-container" class="avatar">
     <div class="skeleton"></div>
-    <img loading="lazy" src="images/avatars/{{$user->avatar ?? "default_user.svg"}}" alt="">
+    <img loading="lazy" src="/images/avatars/{{ $user->avatar ?? 'default_user.svg' }}" alt="">
 </div>
